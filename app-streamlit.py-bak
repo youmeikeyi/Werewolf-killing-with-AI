0fd8a687fd7f9c5695f1a2457923fb9de0aad328
Junco <xujinchao@sina.cn>
2024-06-10 17:17:48 +0800

    update app

---
+++ app-streamlit.py-bak
@@ -0,0 +1,22 @@
+import os
+import streamlit as st
+
+def main():
+
+    st.write("# Welcome to Streamlit! 👋")
+
+
+
+    #AT:ee3ecc6b6cc257755dfe60af7f82dc98b9fed765
+    # download internlm2 to the base_path directory using git tool
+    base_path = './Werewolf-Kill-With-AI'
+    os.system(f'git clone https://Junco:ee3ecc6b6cc257755dfe60af7f82dc98b9fed765@code.openxlab.org.cn/Junco/Werewolf-Kill-With-AI.git {base_path}')
+    os.system(f'git lfs install')
+    os.system(f'cd {base_path} && git lfs pull')
+
+    #/home/xlab-app-center
+    os.system('streamlit run ./web_demo.py --server.address=0.0.0.0 --server.port 7860')
+
+
+if __name__ == "__main__":
+    main()
--- app.py
+++ app.py
@@ -1,22 +1,27 @@
+import gradio as gr
 import os
-import streamlit as st
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
-def main():
+# download internlm2 to the base_path directory using git tool
+# base_path = './internlm2-chat-7b'
+base_path = './Werewolf-Kill-With-AI'
+# download repo to the base_path directory using git
+os.system('apt install git')
+os.system('apt install git-lfs')
+os.system(f'git clone https://code.openxlab.org.cn/Junco/Werewolf-Kill-With-AI.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 
-    st.write("# Welcome to Streamlit! 👋")
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
 
-
-    #AT:ee3ecc6b6cc257755dfe60af7f82dc98b9fed765
-    # download internlm2 to the base_path directory using git tool
-    base_path = './Werewolf-Kill-With-AI'
-    os.system(f'git clone https://Junco:ee3ecc6b6cc257755dfe60af7f82dc98b9fed765@code.openxlab.org.cn/Junco/Werewolf-Kill-With-AI.git {base_path}')
-    os.system(f'git lfs install')
-    os.system(f'cd {base_path} && git lfs pull')
-
-    #/home/xlab-app-center
-    os.system('streamlit run ./web_demo.py --server.address=0.0.0.0 --server.port 7860')
-
-
-if __name__ == "__main__":
-    main()
+gr.ChatInterface(chat,
+                 title="InternLM2-Chat-7B",
+                description="""
+InternLM is mainly developed by Shanghai AI Laboratory.
+                 """,
+                 ).queue(1).launch()
